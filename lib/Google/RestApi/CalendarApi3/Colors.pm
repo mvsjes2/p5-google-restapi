9f40761c9fe228241fcc11964651bfa763feba6f
mvsjes2 <nibor.yarrum@gmail.com>
2026-02-14 23:51:41 -0400

    version bump

--- lib/Google/RestApi/CalendarApi3/Colors.pm
+++ lib/Google/RestApi/CalendarApi3/Colors.pm
@@ -1,6 +1,6 @@
 package Google::RestApi::CalendarApi3::Colors;
 
-our $VERSION = '1.2.0';
+our $VERSION = '2.0.0';
 
 use Google::RestApi::Setup;
 
--- lib/Google/RestApi/SheetsApi4/Range/Row.pm
+++ lib/Google/RestApi/SheetsApi4/Range/Row.pm
@@ -1,6 +1,6 @@
 package Google::RestApi::SheetsApi4::Range::Row;
 
-our $VERSION = '1.2.0';
+our $VERSION = '2.0.0';
 
 use Google::RestApi::Setup;
 
